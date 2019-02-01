f7fac308c11e6b628200b971c32e5b4fea2e211c
Marianne Paulson <marianne@paulsons.info>
2019-02-01 12:11:01 -0800

    2nd commit to lab typical

--- lab1/gumball-java-typical/GumballMachine.java
+++ lab1/gumball-java-typical/GumballMachine.java
@@ -1,43 +1,61 @@
-
+import java.util.ArrayList;
 public class GumballMachine
 {
 
     private int num_gumballs;
-    private boolean has_quarter;
+    private int price;
+    protected ArrayList<Integer> acceptedCoins;
+    private int total = 0;
+    private boolean insertedEnoughMoney;
 
-    public GumballMachine( int size )
+
+    public GumballMachine( int size, int price, ArrayList<Integer> acceptedCoins)
+
     {
         // initialise instance variables
         this.num_gumballs = size;
-        this.has_quarter = false;
+        this.price = price;
+        this.acceptedCoins = new ArrayList<Integer>(acceptedCoins);
+        this.total = 0;
+        this.insertedEnoughMoney = false;
+        
     }
 
-    public void insertQuarter(int coin)
+    public void insertCoin(int coin)
     {
-        if ( coin == 25 )
-            this.has_quarter = true ;
+        if(acceptedCoins.contains(coin)) {
+            total += coin;
+            if ( total >= price )
+                this.insertedEnoughMoney = true ;
+            else 
+                this.insertedEnoughMoney = false ;
+        }
         else 
-            this.has_quarter = false ;
+            System.out.println("Invalid coin");
     }
-    
+
     public void turnCrank()
     {
-    	if ( this.has_quarter )
-    	{
-    		if ( this.num_gumballs > 0 )
-    		{
-    			this.num_gumballs-- ;
-    			this.has_quarter = false ;
-    			System.out.println( "Thanks for your quarter.  Gumball Ejected!" ) ;
-    		}
-    		else
-    		{
-    			System.out.println( "No More Gumballs!  Sorry, can't return your quarter." ) ;
-    		}
-    	}
-    	else 
-    	{
-    		System.out.println( "Please insert a quarter" ) ;
-    	}        
+        if ( this.insertedEnoughMoney )
+        {
+            if ( this.num_gumballs > 0 )
+            {
+                this.num_gumballs-- ;
+                this.insertedEnoughMoney = false ;
+                System.out.println( "Thanks.  Gumball Ejected!" ) ;
+            }
+            else
+            {
+                System.out.println( "No More Gumballs!  Sorry, can't return your money." ) ;
+            }
+        }
+        else 
+        if (total > 0) {
+            System.out.print("Price of gumball is: " + price + ". Please insert more money");
+        }
+        else
+        {
+            System.out.println( "You have not inserted any coins. Please insert money" ) ;
+        }        
     }
 }
--- lab1/gumball-java-typical/Main.java
+++ lab1/gumball-java-typical/Main.java
@@ -1,22 +1,21 @@
-
+import java.util.ArrayList;
+import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) {
-		GumballMachine gumballMachine = new GumballMachine(5);
+    public static void main(String[] args) {
 
-		System.out.println(gumballMachine);
+        GumballMachine gumballMachine = new GumballMachine(5, 50, new ArrayList<Integer>(Arrays.asList(5, 10, 25)));
 
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
+        System.out.println(gumballMachine);
 
-		System.out.println(gumballMachine);
+        gumballMachine.insertCoin( 25 );
+        gumballMachine.insertCoin( 10 );
+        gumballMachine.insertCoin( 10 );
+        gumballMachine.insertCoin( 2 );
+        gumballMachine.insertCoin(5);
+        
+        gumballMachine.turnCrank();
 
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
-
-		System.out.println(gumballMachine);
-	}
+    }
 }
