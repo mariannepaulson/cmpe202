f055c45ae94ca808fb075e26543e172c5952f709
Marianne Paulson <marianne@paulsons.info>
2019-03-23 12:41:46 -0700

    lab7

---
+++ lab7/src/Client.java
@@ -0,0 +1,14 @@
+  
+
+public class Client {
+
+    public static void runTest()
+    {
+        Component theOrder = BuildOrder.getOrder() ;
+        theOrder.printDescription();
+        
+        
+
+    }
+}
+ 
